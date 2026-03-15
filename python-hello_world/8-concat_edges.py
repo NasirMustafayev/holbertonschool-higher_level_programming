b45f0881059c571de4d5d5cf8ad946d3c9138d08
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-03-15 23:25:33 +0400

    Fix

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(str[39:67] + str[107:111] + str[6:7] + str[:6])
+
+print(str[39:67] + str[107:111] + str[6] + str[:6])
