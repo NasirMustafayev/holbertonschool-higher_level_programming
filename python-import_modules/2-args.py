e45701404eade76569b3c1c997bb375fe7f6ac74
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-03-21 23:50:32 +0400

    Task 2, fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,10 +4,13 @@ import sys
 if __name__ == "__main__":
     argc = len(sys.argv) - 1
 
-    if (argc != 1):
-        print("{} arguments".format(argc), end="\n")
+    if argc != 1:
+        if argc == 0:
+            print("0 arguments.")
+        else:
+            print("{} arguments:".format(argc), end="\n")
     else:
-        print("{} argument".format(argc), end="\n")
+        print("{} argument:".format(argc), end="\n")
 
     for i in range(1, argc + 1):
         print("{}: {}".format(i, sys.argv[i]))
