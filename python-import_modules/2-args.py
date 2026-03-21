ba8217a24067cb80aaff00a3abf2c37b7a32b8b9
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-03-21 23:47:53 +0400

    Task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argc = len(sys.argv) - 1
+
+    if (argc != 1):
+        print("{} arguments".format(argc), end="\n")
+    else:
+        print("{} argument".format(argc), end="\n")
+
+    for i in range(1, argc + 1):
+        print("{}: {}".format(i, sys.argv[i]))
