b49c3fe23e250a70a7ef525fc7a52dc4ecd98242
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-03-27 16:52:24 +0400

    Task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python3
 
-def roman_to_int(roman_string)
-    
+def roman_to_int(roman):
+    values = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
+    result = 0
+
+    for i in range(len(roman)):
+        current = values[roman[i]]
+
+        if i + 1 < len(roman) and current < values[roman[i + 1]]:
+            result -= current   # subtractive case (IV, IX, XL...)
+        else:
+            result += current   # normal case
+
+    return result
