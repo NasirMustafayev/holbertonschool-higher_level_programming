6ae4134487f2ba4c9dc2fd6b8a8aee8311a5ac79
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-03-28 16:12:10 +0400

    Task 2, fix

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -9,8 +9,6 @@ def safe_print_list_integers(my_list=[], x=0):
             counter += 1
         except (TypeError, ValueError):
             continue
-        except IndexError:
-            break
 
     print()
     return counter
