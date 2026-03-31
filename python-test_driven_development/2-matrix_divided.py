a3951316ac81d794254a1ad5c00b1f947912775e
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-03-31 14:13:00 +0400

    Task 2, fix missing tests added pycodestyle completed

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """
-This module defines a function that divides all elements of a matrix by a given divisor.
+Defines a function that divides all elements of a matrix.
 >>> matrix = [[1, 2, 3], [4, 5, 6]]
 >>> matrix_divided(matrix, 2)
 [[0.5, 1.0, 1.5], [2.0, 2.5, 3.0]]
 """
+
+
 def matrix_divided(matrix, div):
     """
     Divides all elements of a matrix by a given divisor.
     """
     result = []
-
+    msg = "matrix must be a matrix (list of lists) of integers/floats"
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
@@ -20,14 +22,14 @@ def matrix_divided(matrix, div):
         new_row = []
 
         if not isinstance(row, list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(msg)
         if len(row) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
-            
+
         for num in row:
             if not isinstance(num, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(msg)
             new_row.append(round(num / div, 2))
         result.append(new_row)
-    
+
     return result
