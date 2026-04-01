d380e2e1f2b0774577d7d25dd25a72db2126018c
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-04-01 12:04:13 +0400

    Task 5, documentation and pycodestyle fix

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
+"""
+Module 5-text_indentation
+>>>text_indentation("Line 1. Line 2? Line 3: Line 4")
+Line 1.
+
+Line 2?
+
+Line 3:
+
+Line 4
+"""
+
 
 def text_indentation(text):
+    """
+    Print a text with 2 new lines after each of these characters: ., ? and :
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
@@ -9,11 +24,11 @@ def text_indentation(text):
     for i in range(len(text)):
         if skip and ' ' in text[i]:
             continue
-        if skip and not ' ' in text[i]:
+        if skip and ' ' not in text[i]:
             skip = False
-        
+
         if text[i] in (".", "?", ":"):
-            print(text[i],end="\n\n")
+            print(text[i], end="\n\n")
             skip = True
         else:
             print(text[i], end="")
