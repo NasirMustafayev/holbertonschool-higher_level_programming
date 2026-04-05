a3e93351b8b1b3aaa252c37a7d76ed61273e8333
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-04-05 11:34:06 +0400

    Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module defines a class named Square
+"""
+
+
+class Square:
+    """Defining an object Square with a private instance attribute size"""
+    def __init__(self, size):
+        self.__size = size
