51959e2bea104c34720367ea406ed950eedf80ee
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-04-13 22:43:34 +0400

    Task 2, fix

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,4 +6,4 @@ Defining a function for instance check
 
 def is_same_class(obj, a_class):
     """Returns True if the obj is instance of a_class"""
-    return isinstance(obj, a_class)
+    return type(obj) == a_class
