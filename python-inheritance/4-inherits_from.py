0ff39d43ee750ad81ef4b77a81d74e960ee9516a
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-04-14 01:10:13 +0400

    Task 4, fix

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""
-Defining a function for kind of instance check
-"""
+"""Defines a function that checks inheritance"""
 
 
 def inherits_from(obj, a_class):
-    """Return True if the obj is an instance of a class that inherited"""
-    return issubclass(obj, a_class)
+    """Returns True if obj is instance of inherited class"""
+    return isinstance(obj, a_class) and type(obj) is not a_class
