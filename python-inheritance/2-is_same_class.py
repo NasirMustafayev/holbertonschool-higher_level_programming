9b94280c004219b5ef8d29bb2a1558f99bfff7f8
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-04-14 01:00:06 +0400

    Task 3

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
-Defining a function for instance check
+Defining a function for exact instance check
 """
 
 
 def is_same_class(obj, a_class):
-    """Returns True if the obj is instance of a_class"""
+    """Returns True if the obj is exact of a_class"""
     return type(obj) is a_class
---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Defining a function for kind of instance check
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return True if the obj is instance of a_class"""
+    return isinstance(obj, a_class)
