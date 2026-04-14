a1ba7152008abc46525bf9ac92efa5b1f6f7b27a
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-04-14 16:41:54 +0400

    Task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Defines empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Raising an area exception"""
+    def area(self):
+        raise Exception("area() is not implemented")
