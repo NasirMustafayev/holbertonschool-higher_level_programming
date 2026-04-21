8e80a4993c47169abb54600a41f623be37b7a24b
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-04-21 16:35:35 +0400

    Remove redundant loading of elements from JSON file in add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,7 +11,6 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-elements = load_from_json_file(filename)
 
 if os.path.exists(filename):
     elements = load_from_json_file(filename)
