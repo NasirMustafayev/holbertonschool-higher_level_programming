42dda739a0e2ffbc1193043030bcd47d2cf60ff8
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-04-21 12:22:34 +0400

    Fix formatting by adding a blank line in load_from_json_file.py

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -4,6 +4,7 @@ This module defines a function that creates an Object from a “JSON file”.
 """
 import json
 
+
 def load_from_json_file(filename):
     """Creates an Object from a “JSON file”."""
     with open(filename, "r") as file:
