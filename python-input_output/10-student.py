f9730408acaf30742a90285b8deb3390d9cf5a25
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-04-23 21:38:17 +0400

    Fix to_json method to ensure attrs is a list of strings before processing

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -19,11 +19,13 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        if attrs:
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
             result = {}
 
             for key in attrs:
                 if key in self.__dict__:
                     result[key] = self.__dict__[key]
 
-        return result
+            return result
+
+        return self.__dict__
