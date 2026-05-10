82bf04176defd657e08c8df0700d477ac21c616f
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-05-10 23:57:37 +0400

    Refactor factorial function to handle NaN and simplify code structure

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,18 +1,10 @@
 #!/usr/bin/node
 
-function factorial (n) {
-  if (n === 0 || n === 1) {
+const factorial = (n) => {
+  if (isNaN(n) || n === 0 || n === 1) {
     return 1;
-  } else {
-    return n * factorial(n - 1);
   }
-}
+  return n * factorial(n - 1);
+};
 
-const arg = process.argv[2];
-const convert = +arg;
-
-if (isNaN(convert) || convert < 0) {
-  console.log('NaN');
-} else {
-  console.log(factorial(convert));
-}
+console.log(factorial(parseInt(process.argv[2])));
